3fc6015228d3ba50b2905e5d543f9b5d3c9a35f2
Joey Z <joeyi.zhou@mail.utoronto.ca>
2018-01-13 15:59:13 -0500

    Modelling fixup

--- solution/Part2/Modelling.py
+++ solution/Part2/Modelling.py
@@ -2,22 +2,31 @@ import math
 from solution.Part2.Counting import count
 
 
-def moreEnglish(inString1, inString2, lambdas):
+def moreEnglish(dataset, inString1, inString2, lambdas):
     countTable = {}
-    dataset = open('../../ptb.train.txt', 'r')
     score1 = score(dataset, inString1, countTable, lambdas)
     score2 = score(dataset, inString2, countTable, lambdas)
+    print(score1)
+    print(score2)
+    return (score1 > score2)
 
 def prob(dataset, inString, countTable, lambdas):
     ngrams = [inString[:i] for i in range(len(inString))]
-    sum = 0
+    sum = 0.0
     for i in range(1, len(ngrams)):
-        sum += lambdas[i-1]*count(dataset, ngrams[i], countTable)/count(dataset, ngrams[i-1], countTable)
+        # lambdas[i-1]*
+       if(count(dataset, ngrams[i-1], countTable) != 0): sum += count(dataset, ngrams[i], countTable)/count(dataset, ngrams[i-1], countTable)
     return sum
 
 def score(dataset, inString, countTable, lambdas):
     words = [inString for inString in inString.split(' ')]
-    sum = 0
+    sum = 0.0
     for i in range(0, len(words)):
-        sum += math.log10(prob(dataset, words[i], countTable, lambdas))
+        if(prob(dataset, words[i], countTable, lambdas) > 0.0): sum += math.log10(prob(dataset, words[i], countTable, lambdas))
     return sum
+
+inString1 = "BLAH BL A BLAH"
+inString2 = "THIS IS A TEST"
+lambdas = [1e-6,1e-5,1e-4,1e-3,1e-2,1e-1,0.888889]
+dataset = open('../../ptb.train.txt', 'r').read()
+print(moreEnglish(dataset, inString1, inString2, lambdas))
