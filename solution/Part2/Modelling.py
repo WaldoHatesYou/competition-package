c43544c47d7db1112ae34d6445619c35b56c898c
Joey Z <joeyi.zhou@mail.utoronto.ca>
2018-01-13 16:40:33 -0500

    debugging modelling

--- solution/Part2/Modelling.py
+++ solution/Part2/Modelling.py
@@ -1,28 +1,31 @@
 import math
 from solution.Part2.Counting import count
 
-
+# Calculate score of each string return if string1 score is larger that string2 score
 def moreEnglish(dataset, inString1, inString2, lambdas):
     countTable = {}
     score1 = score(dataset, inString1, countTable, lambdas)
     score2 = score(dataset, inString2, countTable, lambdas)
-    print(score1)
-    print(score2)
+
     return (score1 > score2)
 
 def prob(dataset, inString, countTable, lambdas):
-    ngrams = [inString[:i] for i in range(len(inString))]
+    ngrams = [inString[:i] for i in range(1,len(inString)+1)]
     sum = 0.0
     for i in range(1, len(ngrams)):
         # lambdas[i-1]*
-       if(count(dataset, ngrams[i-1], countTable) != 0): sum += count(dataset, ngrams[i], countTable)/count(dataset, ngrams[i-1], countTable)
+        currTerm = float(count(dataset, ngrams[i], countTable))
+        prevTerm = float(count(dataset, ngrams[i-1], countTable))
+        if(prevTerm > 0.0):
+            sum += currTerm/prevTerm
     return sum
 
+# Sum up the score
 def score(dataset, inString, countTable, lambdas):
     words = [inString for inString in inString.split(' ')]
     sum = 0.0
     for i in range(0, len(words)):
-        if(prob(dataset, words[i], countTable, lambdas) > 0.0): sum += math.log10(prob(dataset, words[i], countTable, lambdas))
+        sum += math.log10(prob(dataset, words[i], countTable, lambdas))
     return sum
 
 inString1 = "BLAH BL A BLAH"
