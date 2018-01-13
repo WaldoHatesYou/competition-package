ef60eeeef321946635b3f30360a83cb056d8108a
Javed Nissar <javednissar@Javeds-MacBook-Air.local>
2018-01-13 13:27:16 -0500

    added general data runner class

---
+++ solution/Part1/DataRunner.py
@@ -0,0 +1,25 @@
+class DataRunner:
+    def __init__(self,data_filename):
+        with open(data_filename, "r") as data_file:
+            self.data_lines = data_file.readlines()
+
+        self.parameters = []
+        for line in self.data_lines:
+            parameter_list = line.split(' | ')
+            self.parameters.append((parameter_list[0], parameter_list[1], parameter_list[2]))
+    def run(self, out_filename, encryption_function, decryption_function):
+        outputs = []
+
+        for parameter in self.parameters:
+            TYPE, key, text = parameter
+            if TYPE == ENCRYPT:
+                outputs.append(encryption_function(text, key))
+            else:
+                outputs.append(decryption_function(text, key))
+
+        with open(out_filename, 'w') as out_file:
+            for output in outputs:
+                out_file.write("%s" % output)
+
+ENCRYPT = "ENCRYPT"
+DECRYPT = "DECRYPT"
