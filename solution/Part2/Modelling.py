8a20a0c153379c1879dca14695ab5998e175ab4c
Javed Nissar <javednissar@Javeds-MacBook-Air.local>
2018-01-13 14:38:47 -0500

    added ngram capture to the probability function

---
+++ solution/Part2/Modelling.py
@@ -0,0 +1,5 @@
+def prob(string, count_table, lambdas):
+    #TODO check that it also puts the whole string in ngrams
+    #PLEASE DO IT JOEY
+    ngrams = [string[:i] for i in range(len(string))]
+
